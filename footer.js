54e299c90823447e403f63905ea852132b11627e
bernamorenogithub <199148669+bernmoreno@users.noreply.github.com>
2026-06-04 15:14:10 -0400

    Update footer email to cleanitflipit@gmail.com

--- footer.js
+++ footer.js
@@ -90,7 +90,7 @@
                   <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"/>
                   <polyline points="22,6 12,13 2,6"/>
                 </svg>
-                hello@cleanitflipit.com
+                cleanitflipit@gmail.com
               </li>
               <li>
                 <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true" style="flex-shrink:0">
